dd99cbff747b98496037c97c56057db03e6bc526
saiyngali <myrzasaiyngali@gmail.com>
2023-10-27 03:35:23 +0600

    Add getting own students for coaches

--- stats/coach/views.py
+++ stats/coach/views.py
@@ -2,6 +2,11 @@ from rest_framework.permissions import AllowAny
 from user.views import UserViewsets
 from .models import Coach
 from .serializers import CoachSerializer
+from student.models import Student
+from student.serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import filters, status
 
 
 class CoachViewsets(UserViewsets):
@@ -9,3 +14,12 @@ class CoachViewsets(UserViewsets):
     serializer_class = CoachSerializer
     permission_classes = [AllowAny]
     http_method_names = ['get', 'list']
+    
+
+    @action(detail=True, methods=['get'], url_name='students')
+    def students(self, request, pk=None):
+        coach = self.get_object()
+        students = Student.objects.filter(coach=coach)
+        serializer = StudentSerializer(students, many=True)
+        
+        return Response({"message": serializer.data}, status=status.HTTP_200_OK)
