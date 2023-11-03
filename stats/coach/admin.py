4b7c6d4cd7c79430ba5b20c3bbc989ef97db9f46
saiyngali <myrzasaiyngali@gmail.com>
2023-11-03 23:38:33 +0600

    Add club field

--- stats/coach/admin.py
+++ stats/coach/admin.py
@@ -7,9 +7,9 @@ class CoachAdmin(admin.ModelAdmin):
     exclude = ('role', 'is_staff', 'is_active', 'is_admin', 'last_login', 'groups', 'is_superuser', 'user_permissions')
 
     fieldsets = (
-        (None, {"fields": ("username", "password", "phone_number", "first_name", "last_name", "image")}),
+        (None, {"fields": ("username", "password", "phone_number", "first_name", "last_name", "image", "club")}),
     )
-    list_display = ("username", "phone_number", "first_name", "last_name", "image")
+    list_display = ("username", "phone_number", "first_name", "last_name", "image", "club")
 
     def save_model(self, request, obj, form, change):
         obj.role = "coach"
