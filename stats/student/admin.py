9a7a48f8ca1da11f96888960eb68f91cca4c8d37
saiyngali <myrzasaiyngali@gmail.com>
2023-11-26 23:14:09 +0600

    Add Last republic winner

--- stats/student/admin.py
+++ stats/student/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, LastRepublicWinner
 
 admin.site.register(Student)
+admin.site.register(LastRepublicWinner)
